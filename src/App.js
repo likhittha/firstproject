a3224be23547befdb04cdff862d0fe1ca6b88c52
likhittha <likhithathammareddylikhitha@gmail.com>
2024-04-19 11:55:52 +0530

    initial commit
    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './navbar/Navbar';
+
+function App() {
+  return (
+   <div className='first'>first app
+  <Navbar/>
+   </div>
+
+
+
+
+
+  );
+}
+
+export default App;
---
+++ src/navbar/Navbar.js
@@ -0,0 +1,25 @@
+import "./Navbar.css"
+export default function Navbar(){
+const name = "likhitha";
+const numberid = "123";
+let data = {
+    name : "likhi" , 
+    number : 123 ,
+    group : "music" ,
+
+
+}
+
+    return (<>
+      <div className="Nav"> This is Navbar {name}  {numberid} </div>
+    <div className="Two" > <div> { data.name } </div>
+                        <div>  {data.number} </div>  
+                          <div> {data.group} </div> 
+
+    </div>
+    </>
+  
+    
+    );
+    
+}
