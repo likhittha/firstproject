838dbbbc55da2698e7b0d531775e845bbfe3e095
Veer-Vamshi01 <146912286+vamshiVeer@users.noreply.github.com>
2024-04-19 13:17:49 +0530

    updated name
    updated name

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -1,25 +1,25 @@
-import "./Navbar.css"
-export default function Navbar(){
-const name = "likhitha";
-const numberid = "123";
-let data = {
-    name : "likhi" , 
-    number : 123 ,
-    group : "blue" ,
+import "./Navbar.css";
+export default function Navbar() {
+  const name = "likhitha";
+  const numberid = "123";
+  let data = {
+    name: "likhi (pandhi)",
+    number: 123,
+    group: "blue",
+  };
 
-
-}
-
-    return (<>
-      <div className="Nav"> This is Navbar {name}  {numberid} </div>
-    <div className="Two" > <div> { data.name } </div>
-                        <div>  {data.number} </div>  
-                          <div> {data.group} </div> 
-
-    </div>
+  return (
+    <>
+      <div className="Nav">
+        {" "}
+        This is Navbar {name} {numberid}{" "}
+      </div>
+      <div className="Two">
+        {" "}
+        <div> {data.name} </div>
+        <div> {data.number} </div>
+        <div> {data.group} </div>
+      </div>
     </>
-  
-    
-    );
-    
+  );
 }
