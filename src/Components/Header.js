82adbce4915153bb4dc595ee8fa19e923109904d
Veer-Vamshi01 <146912286+vamshiVeer@users.noreply.github.com>
2024-04-20 13:09:04 +0530

    Created basic components
    Created basic components

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+export const Header = () => {
+  return (
+    <>
+      <div className="outerDiv">
+        <h1>Header</h1>
+        <div></div>
+      </div>
+      <h1>veer</h1>
+    </>
+  );
+};
